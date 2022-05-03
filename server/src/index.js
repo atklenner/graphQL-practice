6b8e3e2a9e08dd230fca55623020ed4dcad79deb
Andrew <atklenner@gmail.com>
2022-05-03 14:21:01 -0700

    Add some mutations

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
 let links = [
   {
@@ -8,33 +10,43 @@ let links = [
   },
 ];
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      let linkToUpdate = links.find((link) => link.id === args.id);
+      if (args.url) {
+        linkToUpdate.url = args.url;
+      }
+      if (args.description) {
+        linkToUpdate.description = args.description;
+      }
+      return linkToUpdate;
+    },
+    deleteLink: (parent, args) => {
+      let deletedLink = links.filter((link) => link.id === args.id);
+      links = links.filter((link) => link.id !== args.id);
+      return deletedLink[0];
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
 });
 
